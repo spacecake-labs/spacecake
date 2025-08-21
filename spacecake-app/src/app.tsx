ec51afc4fce60a0d565685956813f7249e225f10
code-alexander <152750039+code-alexander@users.noreply.github.com>
2025-08-21 14:20:18 +1000

    chore: fix import syntax

--- spacecake-app/src/app.tsx
+++ spacecake-app/src/app.tsx
@@ -7,7 +7,7 @@ import {
 } from "@tanstack/react-router";
 
 // Import the generated route tree
-import { routeTree } from "./routeTree.gen";
+import { routeTree } from "@/routeTree.gen";
 import { useTheme } from "@/components/theme-provider";
 
 // Create a new router instance with hash routing
