52a4195c60856b8c5dba9bfbbf326ed5a09ba141
code-alexander <152750039+code-alexander@users.noreply.github.com>
2025-10-20 11:37:50 +1000

    perf: optimise with search param selector

--- spacecake-app/src/components/editor/plugins.tsx
+++ spacecake-app/src/components/editor/plugins.tsx
@@ -12,7 +12,6 @@ import { MarkdownShortcutPlugin } from "@lexical/react/LexicalMarkdownShortcutPl
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin"
 import { TabIndentationPlugin } from "@lexical/react/LexicalTabIndentationPlugin"
 import { TablePlugin } from "@lexical/react/LexicalTablePlugin"
-import { atom, useAtom } from "jotai"
 
 import { ContentEditable } from "@/components/editor/content-editable"
 import { BackspacePreventionPlugin } from "@/components/editor/plugins/backspace-prevention"
@@ -21,24 +20,14 @@ import { SaveCommandPlugin } from "@/components/editor/plugins/save-command"
 import { SlashCommandPlugin } from "@/components/editor/plugins/slash-command"
 import { MARKDOWN_TRANSFORMERS } from "@/components/editor/transformers/markdown"
 
-const floatingAnchorAtom = atom<HTMLDivElement | null>(null)
-
 export const Plugins = React.memo(function Plugins() {
-  const [, setFloatingAnchorElem] = useAtom(floatingAnchorAtom)
-
-  const onRef = (_floatingAnchorElem: HTMLDivElement) => {
-    if (_floatingAnchorElem !== null) {
-      setFloatingAnchorElem(_floatingAnchorElem)
-    }
-  }
-
   return (
     <div className="relative">
       <div className="relative">
         <RichTextPlugin
           contentEditable={
             <div className="h-full flex flex-1 min-h-0">
-              <div className="h-full flex-1 min-h-0" ref={onRef}>
+              <div className="h-full flex-1 min-h-0">
                 <ContentEditable
                   placeholder={""}
                   className="ContentEditable__root relative block h-full min-h-0 flex-1 overflow-auto px-8 py-4 focus:outline-none"
