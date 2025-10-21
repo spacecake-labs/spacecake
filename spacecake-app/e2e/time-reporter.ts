df781e64770519bce5f7e78551e570cd513ec676
code-alexander <152750039+code-alexander@users.noreply.github.com>
2025-10-22 08:57:41 +1000

    test: adding time reporter

---
+++ spacecake-app/e2e/time-reporter.ts
@@ -0,0 +1,22 @@
+import type {
+  FullResult,
+  Reporter,
+  TestCase,
+  TestResult,
+} from "@playwright/test/reporter"
+
+class TimeReporter implements Reporter {
+  onTestEnd(_test: TestCase, result: TestResult) {
+    const duration = (result.duration / 1000).toFixed(2)
+    console.log(`\t⏳: \x1b[32m${duration}s\x1b[0m`)
+  }
+
+  onEnd(result: FullResult) {
+    const duration = (result.duration / 1000).toFixed(2)
+    console.log(`\t⌛ total: \x1b[32m${duration}s\x1b[0m`)
+  }
+}
+
+export default TimeReporter
+
+export { TimeReporter }
