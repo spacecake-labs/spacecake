3a557d3036685e7e08d6f29c8082d9675536e665
code-alexander <152750039+code-alexander@users.noreply.github.com>
2025-10-22 08:57:41 +1000

    test: adding time reporter

--- spacecake-app/playwright.config.ts
+++ spacecake-app/playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   // workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "list",
+  reporter: [["list"], ["./e2e/time-reporter"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
