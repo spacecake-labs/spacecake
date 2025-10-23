05b14d08d4f2422ab7f2c3e961faa3d01e01f8a7
code-alexander <152750039+code-alexander@users.noreply.github.com>
2025-10-23 12:59:50 +1000

    deps: upgrade lexical

---
+++ spacecake-app/src/schema/workspace-cache.ts
@@ -0,0 +1,20 @@
+import { FilePrimaryKeySchema } from "@/schema/file"
+import { Schema } from "effect"
+
+import { ViewKindSchema } from "@/types/lexical"
+import { AbsolutePathSchema } from "@/types/workspace"
+
+/**
+ * Workspace cache row represents the cached file information for a workspace.
+ * This is used to quickly determine file status and editor state on startup.
+ */
+export const WorkspaceCacheRowSchema = Schema.Struct({
+  filePath: AbsolutePathSchema,
+  fileId: FilePrimaryKeySchema,
+  view_kind: ViewKindSchema,
+  has_cached_state: Schema.Boolean,
+  mtime: Schema.String,
+  cid: Schema.String,
+})
+
+export type WorkspaceCacheRow = typeof WorkspaceCacheRowSchema.Type
