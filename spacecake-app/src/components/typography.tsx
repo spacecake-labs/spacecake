38295a8aa024735be94f0ccb8bfca65e631c9259
code-alexander <152750039+code-alexander@users.noreply.github.com>
2025-10-28 09:37:44 +1000

    fix: cursor styling issue in inline code

--- spacecake-app/src/components/typography.tsx
+++ spacecake-app/src/components/typography.tsx
@@ -4,7 +4,7 @@ export function TypographyInlineCode({
   children: React.ReactNode
 }) {
   return (
-    <code className="bg-muted relative rounded px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+    <code className="bg-muted rounded px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
       {children}
     </code>
   )
