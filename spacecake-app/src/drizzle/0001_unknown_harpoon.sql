51656a83280c658418f3eeeef0c0b499b0fa1328
code-alexander <152750039+code-alexander@users.noreply.github.com>
2025-10-31 15:40:29 +1000

    chore: reset drizzle schema

--- spacecake-app/src/drizzle/0001_unknown_harpoon.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "editor" ADD COLUMN "selection" jsonb;
--- spacecake-app/src/drizzle/0002_cynical_otto_octavius.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "editor" ALTER COLUMN "last_accessed_at" DROP DEFAULT;--> statement-breakpoint
-ALTER TABLE "editor" ALTER COLUMN "last_accessed_at" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "file" ALTER COLUMN "last_accessed_at" DROP DEFAULT;--> statement-breakpoint
-ALTER TABLE "file" ALTER COLUMN "last_accessed_at" DROP NOT NULL;
