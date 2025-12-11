742cd17c910110af8534ed89783aa5d8f63f0d24
code-alexander <152750039+code-alexander@users.noreply.github.com>
2025-12-11 17:50:32 +1000

    feat: initial website

---
+++ website/astro.config.mjs
@@ -0,0 +1,37 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "spacecake",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/spacecake-labs/spacecake-releases",
+        },
+      ],
+      pagefind: false,
+      //   sidebar: [
+      //     {
+      //       label: "Guides",
+      //       items: [
+      //         // Each item here is one entry in the navigation menu.
+      //         { label: "Example Guide", slug: "guides/example" },
+      //       ],
+      //     },
+      //     {
+      //       label: "Reference",
+      //       autogenerate: { directory: "reference" },
+      //     },
+      //   ],
+      components: {
+        Hero: "./src/components/Hero.astro",
+      },
+      customCss: ["./src/styles/theme.css"],
+    }),
+  ],
+});
