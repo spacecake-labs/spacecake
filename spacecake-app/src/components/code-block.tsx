158f62e3becdab64d923e73cd40c5a50e227fd4a
code-alexander <152750039+code-alexander@users.noreply.github.com>
2025-12-17 11:00:48 +1000

    fix: disable language switcher in code files

--- spacecake-app/src/components/code-block.tsx
+++ spacecake-app/src/components/code-block.tsx
@@ -4,10 +4,12 @@ import { Code } from "lucide-react"
 import type { LanguageSpec } from "@/types/language"
 import { LANGUAGE_SUPPORT } from "@/types/language"
 import type { Block } from "@/types/parser"
+import { FileType } from "@/types/workspace"
 import { blockId } from "@/lib/parser/block-id"
 import { delimitPythonDocString } from "@/lib/parser/python/utils"
 import { cn } from "@/lib/utils"
 import { fileTypeEmoji, fileTypeFromLanguage } from "@/lib/workspace"
+import { useRoute } from "@/hooks/use-route"
 import { Badge } from "@/components/ui/badge"
 import {
   Select,
@@ -42,8 +44,10 @@ export function CodeBlock({
   children,
   codeBlockContext,
 }: CodeBlockProps) {
-  // this means it's a code block in a markdown file
-  const canChangeLanguage = block.kind === "code"
+  const route = useRoute()
+
+  // can change language only if the file is markdown; disabled for code files
+  const canChangeLanguage = route?.fileType === FileType.Markdown
 
   const code = block.text
   const blockName = block.name.value
