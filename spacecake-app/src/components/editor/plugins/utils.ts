d7d117ef3c2bf8821b0c0eda07fd4793c5fa1a7d
code-alexander <152750039+code-alexander@users.noreply.github.com>
2025-12-29 08:49:00 +1000

    chore: remove unused context node

--- spacecake-app/src/components/editor/plugins/utils.ts
+++ spacecake-app/src/components/editor/plugins/utils.ts
@@ -1,14 +1,13 @@
 import { $isDecoratorNode, $isRootOrShadowRoot, LexicalNode } from "lexical"
 
-import { $isContextNode } from "@/components/editor/nodes/context-node"
-
 /**
  * Returns true if a spacer is needed after this node.
  * A spacer is needed if the node is a decorator, or if it's a shadow root and
  * its last child is a decorator.
  */
 export function needsSpacer(node: LexicalNode): boolean {
-  if ($isDecoratorNode(node) || $isContextNode(node)) {
+  // add || $isContextNode(node) here if used in future
+  if ($isDecoratorNode(node)) {
     return true
   }
   if ($isRootOrShadowRoot(node)) {
