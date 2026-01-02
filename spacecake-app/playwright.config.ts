eab12ef9a6933fe89a34c59889ddf37b650be530
code-alexander <152750039+code-alexander@users.noreply.github.com>
2026-01-03 08:43:24 +1000

    chore: limit playwright ci failures

--- spacecake-app/playwright.config.ts
+++ spacecake-app/playwright.config.ts
@@ -13,7 +13,8 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  // workers: 1,
+  // Stop tests after n failures
+  maxFailures: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [["list"], ["./e2e/time-reporter"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
