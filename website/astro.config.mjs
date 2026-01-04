9c65d2f08a2924eaed8e1ac33366eefe01071d11
code-alexander <152750039+code-alexander@users.noreply.github.com>
2026-01-05 08:53:40 +1000

    chore: move repo to org

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
         {
           icon: "github",
           label: "GitHub",
-          href: "https://github.com/spacecake-labs/spacecake-releases",
+          href: "https://github.com/spacecake-labs/spacecake",
         },
       ],
       pagefind: false,
