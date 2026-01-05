3564c373ca87d1aaa49c4edda0e92ec57837684b
code-alexander <152750039+code-alexander@users.noreply.github.com>
2026-01-05 16:16:31 +1000

    fix: terminal path

---
+++ spacecake-app/src/main-process/fix-path.ts
@@ -0,0 +1,25 @@
+import process from "node:process"
+
+import { shellPath } from "shell-path"
+import stripAnsi from "strip-ansi"
+
+export async function fixPath() {
+  if (process.platform === "win32") {
+    return
+  }
+
+  try {
+    const pathEnv = await shellPath()
+
+    process.env.PATH =
+      (pathEnv ? stripAnsi(pathEnv) : undefined) ||
+      [
+        "./node_modules/.bin",
+        "/.nodebrew/current/bin",
+        "/usr/local/bin",
+        process.env.PATH,
+      ].join(":")
+  } catch (e) {
+    console.error("Failed to fix path:", e)
+  }
+}
